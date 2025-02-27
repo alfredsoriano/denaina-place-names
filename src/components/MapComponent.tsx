b1500253208c675317b2f322de427d45e94e2031
alfredsoriano <akcsoriano@gmail.com>
2025-02-27 12:13:20 -0900

    fixed zoom level, minZoom, and set bounds

--- src/components/MapComponent.tsx
+++ src/components/MapComponent.tsx
@@ -8,11 +8,12 @@ const MapComponent: React.FC = () => {
   const [denainaLocationsMock] = useState<DenainaLocation[]>(denainaLocationsMockData as DenainaLocation[]);
   
   return (
-    <MapContainer center={[61.2176, -149.8997]} zoom={13} style={{ height: '80vh', width: '100vw' }}>
+    <MapContainer center={[61.2176, -149.8997]} zoom={9} minZoom={7} style={{height: '80vh', width: '100vw' }} maxBounds={[[58.78879, -153.63653], [62.77450, -146.17683]]} maxBoundsViscosity={0.5}>
       <TileLayer
         url='https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
       />
+
       {denainaLocationsMock.map((location) => (
         <Marker
           key={location.id}
