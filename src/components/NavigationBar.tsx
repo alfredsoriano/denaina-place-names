41adc6d92e6560b13ca1ab25e04ef8bf22bcc74a
Jiwon Bae <jiwonbae@Jiwons-MacBook-Pro.local>
2025-03-26 16:59:06 -0800

    Nav bar update

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -1,18 +1,16 @@
+import Nav from "react-bootstrap/Nav";
+import Navbar from "react-bootstrap/Navbar";
+
 function NavigationBar() {
   return (
-    <div
-      style={{
-        color: "white",
-        backgroundColor: "#273B09",
-        height: "50px",
-        padding: "5px 50px",
-        alignContent: "center",
-        fontSize: "20px",
-        fontWeight: "800",
-      }}
-    >
-      Dena'ina Place Names
-    </div>
+    <Navbar style={{ backgroundColor: "#273B09", padding: "5px 50px" }}>
+      <Nav className="w-100 d-flex align-items-center" style={{ gap: "30px", fontSize: "20px", fontWeight: "800" }}>
+        <Nav.Link href="#home" style={{ color: "white" }}>Dena'ina Place Names</Nav.Link>
+        <Nav.Link href="#maps" style={{ color: "white" }}>Maps</Nav.Link>
+        <Nav.Link href="#about" style={{ color: "white" }}>About/About us</Nav.Link>
+        <Nav.Link href="#FAQ" style={{ color: "white" }}>FAQ</Nav.Link>
+      </Nav>
+    </Navbar>
   );
 }
 
