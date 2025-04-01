90909eff00e695921f3ca7bd61733d73a4309cef
Jiwon Bae <jiwonbae@MacBookPro.ht.home>
2025-03-31 23:50:23 -0800

    Big navigation bar Update. Sizing fixed. Now added Collapse

--- src/components/NavigationBar.tsx
+++ src/components/NavigationBar.tsx
@@ -15,45 +15,35 @@ function NavigationBar() {
   }, [darkMode]);
 
   return (
-    <Navbar
-      style={{
-        backgroundColor: darkMode ? "#273B09" : "#DCD7C9",
-        padding: "5px 50px",
-        maxHeight: "100vh",
-        maxWidth: "100vw",
-      }}
-    >
-      <Nav
-        className="w-100 d-flex align-items-center"
-        style={{
-          gap: "30px",
-          fontSize: "20px",
-          fontWeight: "800",
-          overflow: "auto",
-        }}
-      >
-        <Nav.Link href="#home" style={{ color: darkMode ? "white" : "#273B09"}}>
+    <>
+      <Navbar expand="md" style={{ backgroundColor: darkMode ? "#273B09" : "#DCD7C9", fontWeight: "800", padding: "5px 50px" }}>
+        <Navbar.Brand href="#home" style={{ color: darkMode ? "white" : "#273B09" }}>
           Dena'ina Place Names
-        </Nav.Link>
-        <Nav.Link href="#maps" style={{ color: darkMode ? "white" : "#273B09"}}>
-          Maps
-        </Nav.Link>
-        <Nav.Link href="#about" style={{ color: darkMode ? "white" : "#273B09"}}>
-          About
-        </Nav.Link>
-        <Nav.Link href="#FAQ" style={{ color: darkMode ? "white" : "#273B09"}}>
-          FAQ
-        </Nav.Link>
-      </Nav>
-
-      <Button
-        variant="outline-light"
-        onClick={toggleMode}
-        style={{ color: darkMode ? "white" : "#273B09", border: "none" }}
-      >
-        {darkMode ? <BsFillSunFill /> : <BsFillMoonStarsFill />}
-      </Button>
-    </Navbar>
+        </Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbar-nav" />
+        <Navbar.Collapse id="navbar-nav">
+          <Nav className="w-100 d-flex align-items-center" style={{ gap: "30px", fontSize: "20px", fontWeight: "800", overflow: "auto" }}>
+            <Nav.Link href="#home" style={{ color: darkMode ? "white" : "#273B09" }}>
+              Home
+            </Nav.Link>
+            <Nav.Link href="#maps" style={{ color: darkMode ? "white" : "#273B09" }}>
+              Maps
+            </Nav.Link>
+            <Nav.Link href="#about" style={{ color: darkMode ? "white" : "#273B09" }}>
+              About
+            </Nav.Link>
+            <Nav.Link className="flex-grow-1" href="#FAQ" style={{ color: darkMode ? "white" : "#273B09"}}>
+              FAQ
+            </Nav.Link>
+            <Button variant="outline-light" onClick={toggleMode} style={{ fontSize: "20px" , color: darkMode ? "white" : "#273B09", border: "none" }}>
+          {darkMode ? <BsFillSunFill /> : <BsFillMoonStarsFill />}
+        </Button>
+          </Nav>
+          
+        </Navbar.Collapse>
+        
+      </Navbar>
+    </>
   );
 }
 
