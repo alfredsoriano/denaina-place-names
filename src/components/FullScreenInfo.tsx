68f28f341aeeb671452af750edaf7891cb730176
Christopher yang <146152532+Cyang18@users.noreply.github.com>
2025-04-05 15:42:40 -0800

    Update FullScreenInfo.tsx
    fixed the scaling issue with the gallery and add in a background color to the description.

--- src/components/FullScreenInfo.tsx
+++ src/components/FullScreenInfo.tsx
@@ -1,6 +1,5 @@
-import {  useContext, useState } from "react";
-import { Button } from "react-bootstrap";
-import { Carousel } from "react-bootstrap";
+import { useContext, useState } from "react";
+import { Button, Carousel } from "react-bootstrap";
 import { DenainaLocation } from "../types";
 import { ThemeContext } from "../context/ThemeContext";
 
@@ -22,13 +21,13 @@ const FullScreenInfo = ({
     setPlayAudio((prev) => !prev);
   };
 
-  //function to get YouTube embedded video URL
-  const getYouTubeEmbedUrl = (videoUrl: string, autoplay = 0) => {
-    const videoId = videoUrl.split("v=")[1];
-    return `https://www.youtube.com/embed/${videoId}?autoplay=${autoplay}&controls=1`;
+  // function to get YouTube embedded video URL
+  const getYouTubeEmbedUrl = (videoUrl: string, autoplay = 1) => {
+    const videoId = videoUrl.split("v=")[1].split("&")[0]; // Ensure you only get the video ID
+    return `https://www.youtube.com/embed/${videoId}?autoplay=${autoplay}&controls=1&showinfo=0&rel=0`;
   };
 
-  //function to load media type: returns either image or video
+  // function to load media type: returns either image or video
   const loadMedia = (item: string) => {
     let temp: string[] = item.split(".");
     if (temp[1] === "jpg" || temp[1] === "png")
@@ -36,8 +35,10 @@ const FullScreenInfo = ({
         <img
           style={{
             width: "100%",
-            height: "36vh",
+            height: "auto",
             objectFit: "cover",
+            borderRadius: "10px",
+
           }}
           src={item}
         />
@@ -48,7 +49,9 @@ const FullScreenInfo = ({
           style={{
             display: "block",
             width: "100%",
-            height: "36vh",
+            height: "auto",
+            borderRadius: "10px",
+            minHeight: "450px"
           }}
           src={getYouTubeEmbedUrl(location.videoUrl)}
           title={location.title}
@@ -61,7 +64,6 @@ const FullScreenInfo = ({
 
   return (
     // div styling for the pop-up window
-
     <div
       style={{
         position: "absolute",
@@ -73,17 +75,16 @@ const FullScreenInfo = ({
         color: darkTheme ? "#DCD7C9" : "#2C3930",
         display: "flex",
         flexDirection: "column",
-        alignItems: "center",
-        justifyContent: "flex-start",
-        overflowY: "hidden",
         padding: "20px",
+        overflowY: "auto",
         zIndex: 1000,
+        boxSizing: "border-box",
       }}
     >
       {/* Headers
-      <h1> is reserved for the English Location Title Name
-      <h2> is reserved for the Dena'ina Name and Dena'ina Meaning
-       */}
+        <h1> is reserved for the English Location Title Name
+        <h2> is reserved for the Dena'ina Name and Dena'ina Meaning
+      */}
 
       {/* Styling for the background for the title */}
       <div
@@ -99,7 +100,6 @@ const FullScreenInfo = ({
           backgroundSize: "cover",
           backgroundPosition: "center top",
           borderBottom: "5px solid rgba(0, 0, 0, 0.8)",
-          //borderRight: "5px solid rgba(0, 0, 0, 0.8)",
         }}
       >
         <h1>{location.title}</h1>
@@ -125,56 +125,66 @@ const FullScreenInfo = ({
           )}
           {playAudio && location.audioUrl && (
             <iframe
-              width="0%"
+              width="0"
               height="0"
-              src={getYouTubeEmbedUrl(location.audioUrl, playAudio ? 1 : 0)}
+              src={getYouTubeEmbedUrl(location.audioUrl, 1)}
               title={`${location.title} Audio`}
               frameBorder="0"
               allow="autoplay"
-              style={{ marginTop: "20px", display: "block" }}
+              style={{ display: "none" }}
             />
           )}
         </h2>
       </div>
 
-      {/* div style for scrollable container */}
-      <div
-        style={{
-          height: "40vh",
-          minHeight: "30vh",
-          overflow: "auto",
-          marginTop: "20px",
-          marginBottom: "20px",
-        }}
-      >
+      {/* Content section split in two: text on left, carousel on right */}
+      <div>
         {/* div style paragraph text inside scrollable container */}
-        <p
+        <div
           style={{
-            marginLeft: "5%",
-            marginRight: "5%",
-            marginTop: "1%",
+            flex: "1 1 400px",
+            width: "96vw",
+            //maxWidth: "600px",
+            backgroundColor: darkTheme ? "#394D3D" : "#fff",
+            padding: "10px",
+            borderRadius: "10px",
+            boxShadow: "0 2px 6px rgba(0,0,0,0.2)",
+            height: "45vh",
+            minHeight: "30vh",
+            overflow: "auto",
+            marginTop: "20px",
+            marginBottom: "20px",
           }}
         >
-          {location.description ?? "No description found."} <br></br>
-          <br></br> {location.culture ?? "No cultural description found."}
-        </p>
-      </div>
+          <p
+            style={{
+              marginBottom: "1rem",
+            }}
+          >
+            {location.description ?? "No description found."}
+          </p>
+          <p>{location.culture ?? "No cultural description found."}</p>
+        </div>
 
-      {/* react-bootstrap carousel */}
-      <Carousel
-        style={{
-          width: "80vw",
-          maxHeight: "36vh",
-          borderRadius: "10px",
-          overflow: "hidden",
-        }}
-        interval={null} // react-bootstrap: disable auto scrolling
-      >
-        {/* map images and videos and display in carousel */}
-        {location.media.map((item: any, i: any) => (
-          <Carousel.Item key={i}>{loadMedia(item)}</Carousel.Item>
-        ))}
-      </Carousel>
+        {/* react-bootstrap carousel */}
+        <div>
+          <Carousel
+            style={{
+              borderRadius: "10px",
+              overflow: "hidden",
+              boxShadow: "0 2px 6px rgba(0,0,0,0.2)",
+              marginBottom: "80px",
+              width: "96vw", 
+              maxHeight: "65vh",
+            }}
+            interval={null}
+          >
+            {location.media.map((item: any, i: any) => (
+              <Carousel.Item key={i}>{loadMedia(item)}</Carousel.Item>
+            ))}
+          </Carousel>
+        </div>
+      </div>
 
       {/* react-bootstrap button */}
       <Button
